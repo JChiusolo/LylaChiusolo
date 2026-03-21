4139f53717cdbe9a597350ff86cec3ad55b54a09
JChiusolo <jasonchiusolo@gmail.com>
2026-03-21 10:41:18 -0400

    Update SearchPage.jsx

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -19,7 +19,7 @@ export default function SearchPage() {
 
   return (
     <div className="container-max py-8">
-      <h1 className="text-4xl font-bold text-neutral-900 mb-8">Research Search</h1>
+      <h1 className="text-4xl font-bold text-neutral-900 mb-8">Medical Information Search</h1>
 
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
         <div className="lg:col-span-1">
