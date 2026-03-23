9a06feed0fd568ed8723dd2d767d9caee8b7c7eb
JChiusolo <jasonchiusolo@gmail.com>
2026-03-23 12:31:44 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import AuthCallback from './pages/AuthCallback'
-import SearchPage from './pages/SearchPage'
-import HomePage from './pages/HomePage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MOAVisualization from './components/MOAVisualization';
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/auth/callback" element={<AuthCallback />} />
-        <Route path="/" element={<HomePage />} />
+        {/* Existing routes */}
+        <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchPage />} />
+        
+        {/* NEW MOA ROUTES */}
+        <Route path="/moa" element={<MOAVisualization />} />
+        <Route path="/moa/:drug" element={<MOAVisualization />} />
+        
+        {/* Other routes */}
       </Routes>
-    </Router>
-  )
+    </BrowserRouter>
+  );
 }
